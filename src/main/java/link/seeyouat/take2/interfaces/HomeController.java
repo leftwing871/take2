c75f39c5b276c4f09ec809e874a4453638b5b859
MZC01-EHLEE <mzc01-ehlee@MZC01-EHLEE.local>
2022-02-12 16:16:52 +0900

    x

--- src/main/java/link/seeyouat/take2/interfaces/HomeController.java
+++ src/main/java/link/seeyouat/take2/interfaces/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,7 +28,7 @@ public class HomeController {
 
 
     @GetMapping("/environment")
-    public String hello(Model model) throws IOException {
+    public String hello(Model model, @RequestParam(value = "sleep_sec", required = false, defaultValue = "") String sleep_sec) throws IOException, InterruptedException {
 
         //시스템 환경변수 값 전체 가져오기 (key, value 형태)
         Map <String, String> mapX = System.getenv();
@@ -42,23 +43,28 @@ public class HomeController {
         // 환경변수 관리 객체 생성
         ConfigurableEnvironment env = ctx.getEnvironment();
 
-
         // 프로퍼티 정보 얻기
         String ip = env.getProperty("ip");
         String hostName = env.getProperty("hostname");
-        String SLEEP = env.getProperty("SLEEP");
+        String SLEEP_SEC = env.getProperty("SLEEP_SEC");
+        SLEEP_SEC = SLEEP_SEC == null ? "" : SLEEP_SEC;
 
-        System.out.println(ip);
-        System.out.println(hostName);
-        System.out.println(SLEEP);
+        if(!SLEEP_SEC.isEmpty() && !SLEEP_SEC.isBlank())
+        {
+            Thread.sleep(Integer.parseInt(SLEEP_SEC) * 1000);
+        }
 
-        HashMap<String, String> map = new HashMap<>();
-        map.put("ip", ip);
-        map.put("hostname", hostName);
-        map.put("SLEEP", SLEEP);
+        if(!sleep_sec.isEmpty() && !sleep_sec.isBlank())
+        {
+            Thread.sleep(Integer.parseInt(sleep_sec) * 1000);
+        }
 
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("ip", ip);
+//        map.put("hostname", hostName);
+//        map.put("SLEEP_SEC", SLEEP_SEC);
 
-        model.addAttribute("mapENV", map);
+        model.addAttribute("mapENV", mapX);
 
         return "environment";
     }
