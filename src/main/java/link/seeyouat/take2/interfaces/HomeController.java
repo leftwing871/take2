34b2c5a96bc4965b666a0cf282e33535c03b4f4d
MZC01-EHLEE <mzc01-ehlee@MZC01-EHLEE.local>
2022-02-19 07:36:55 +0900

    add raiseRrror

--- src/main/java/link/seeyouat/take2/interfaces/HomeController.java
+++ src/main/java/link/seeyouat/take2/interfaces/HomeController.java
@@ -68,4 +68,43 @@ public class HomeController {
 
         return "environment";
     }
+
+    @GetMapping("/raiseerror")
+    public String raiseError(Model model, @RequestParam(value = "sleep_sec", required = false, defaultValue = "") String sleep_sec) throws IOException, InterruptedException {
+
+        //시스템 환경변수 값 전체 가져오기 (key, value 형태)
+        Map <String, String> mapX = System.getenv();
+        for (Map.Entry <String, String> entry: mapX.entrySet()) {
+            System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
+        }
+
+        // 컨테이너 생성
+        GenericXmlApplicationContext ctx
+                = new GenericXmlApplicationContext();
+
+        // 환경변수 관리 객체 생성
+        ConfigurableEnvironment env = ctx.getEnvironment();
+
+        // 프로퍼티 정보 얻기
+        String ip = env.getProperty("ip");
+        String hostName = env.getProperty("hostname");
+        String SLEEP_SEC = env.getProperty("SLEEP_SEC");
+        SLEEP_SEC = SLEEP_SEC == null ? "" : SLEEP_SEC;
+
+        if(!SLEEP_SEC.isEmpty() && !SLEEP_SEC.isBlank())
+        {
+            Thread.sleep(Integer.parseInt(SLEEP_SEC) * 1000);
+        }
+
+        if(!sleep_sec.isEmpty() && !sleep_sec.isBlank())
+        {
+            Thread.sleep(Integer.parseInt(sleep_sec) * 1000);
+        }
+
+        Integer i = Integer.parseInt("kkk");
+
+        model.addAttribute("mapENV", mapX);
+
+        return "environment";
+    }
 }
